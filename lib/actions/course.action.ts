77c2318fb612a54cb714fe92a1902a4e4d06025f
morteza <mori2002sa@gmail.com>
2024-07-09 13:33:24 +0330

    Create course with server action

---
+++ lib/actions/course.action.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { handleError } from '@/lib/utils';
+import prisma from '@/prisma/client';
+import { TCourseNameFormData } from '@/validation';
+import { auth } from '@clerk/nextjs/server';
+
+export const createCourse = async (data: TCourseNameFormData) => {
+  const { userId } = auth();
+  const { title } = data;
+  try {
+    if (!userId) {
+      return { error: "You're unauthorized! Please login to your account." };
+    }
+
+    const newCourse = await prisma.course.create({
+      data: {
+        title,
+        userId
+      }
+    });
+    return { newCourse };
+  } catch (error) {
+    handleError(error);
+  }
+};
