9049ecb2f8b1082724cff650ae2fab061768ab3d
morteza <mori2002sa@gmail.com>
2024-07-17 17:23:15 +0330

    Add the action cell in the data table

--- app/(dashboard)/_components/courses/Columns.tsx
+++ app/(dashboard)/_components/courses/Columns.tsx
@@ -1,9 +1,16 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 import { Course } from '@prisma/client';
 import { ColumnDef } from '@tanstack/react-table';
-import { ArrowUpDown } from 'lucide-react';
+import { ArrowUpDown, MoreHorizontal, Pencil } from 'lucide-react';
+import Link from 'next/link';
 
 const columns: ColumnDef<Course>[] = [
   {
@@ -45,6 +52,30 @@ const columns: ColumnDef<Course>[] = [
           Published
           <ArrowUpDown className='ml-2 h-4 w-4' />
         </Button>
+      );
+    }
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const { id } = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={'ghost'} className='h-4 w-8 p-0'>
+              <span className='sr-only'></span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align={'end'}>
+            <Link href={`/teacher/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil size={17} className='mr-2' />
+                Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     }
   }
